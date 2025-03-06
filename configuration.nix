1aebf789406b48cde69a7b1fafc9654a1d6b0f2f
Carl Lundin <carllundin55@gmail.com>
2025-03-05 21:21:37 -0800

    Enable playerctld to control media keys.

--- configuration.nix
+++ configuration.nix
@@ -41,6 +41,8 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
+  services.playerctld.enable = true;
+
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
