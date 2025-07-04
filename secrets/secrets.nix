673480d188605a4708bc6c3bb22433170b641c4e
Carl Lundin <carllundin55@gmail.com>
2025-07-04 08:42:45 -0700

    Add PMP secret to laptop.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   loki = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBFOI6pZqWnvS+ePkQkvaKNnzs1sgsXldEcLiWZ/T99t root@loki";
+  freia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFYZmwM5fljuQF2JpA/pPMmFI26wiYJP/+Ka1HIIELTF root@freia";
 in
 {
-  "pmp_key.age".publicKeys = [ loki ];
+  "pmp_key.age".publicKeys = [ loki freia ];
 }
