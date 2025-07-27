150e0205c2e8d954a255280036ae2dec2b559327
Carl Lundin <carllundin55@gmail.com>
2025-07-27 07:21:56 -0700

    Setup RPI

--- systems/rpi/rpi.nix
+++ systems/rpi/rpi.nix
@@ -5,6 +5,9 @@
     ./hardware-configuration.nix
   ];
 
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
+
   networking.hostName = "floki";
   networking.networkmanager.enable = true;
 
@@ -24,5 +27,15 @@
 
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
+
+  nix.settings.trusted-users = [ "carl" ];
+
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
 }
 
