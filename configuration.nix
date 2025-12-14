fa92a9e1c33cfa491d333197461ee87d02c8c02b
Carl Lundin <carllundin55@gmail.com>
2025-12-13 20:49:56 -0800

    Add ssh key

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,7 @@ in
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMPHPeLSIQgoO2MZCxAXoVxaaZVC0hp1oa81cFO3/zDf carl@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIED50DA1QiJerIlFy8Ea04dm1AOWHCrhflNgblREqb8Z carl@freia"
     ];
   };
 
