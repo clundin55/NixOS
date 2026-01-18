7b55406b42dda91427bf692f1b526c768f06b849
Carl Lundin <carllundin55@gmail.com>
2026-01-18 14:18:20 -0800

    Flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Carl Lundin's NixOS systems";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stock-ticker.url = "github:clundin55/stock-ticker";
